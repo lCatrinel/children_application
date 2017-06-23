b15d42aa36258e596f971cf2bcf5f583b128032a
luxrobi <luxrobi@yahoo.com>
2017-06-23 16:42:45 +0300

    No more invalid drops

--- application for children/includes/scripts/runMap.js
+++ application for children/includes/scripts/runMap.js
@@ -30,7 +30,13 @@ function drop(event) {
     var grandparent = event.target.parentElement.parentElement;
 
     var flagIdToRemove = event.dataTransfer.getData("flag");
-    $("#" + flagIdToRemove).animate({ opacity: 0 }); //.fadeOut(); //.hide()
+
+    //check if valid
+    if (isValidCombination(grandparent.className, flagIdToRemove)) {
+        $("#" + flagIdToRemove).animate({ opacity: 0 }); //.fadeOut(); //.hide()
+    } else {
+        alert("invalid drop");
+    }
 }
 
 function disableDrop(event) {
@@ -41,4 +47,53 @@ function disableDrop(event) {
 function disabledDrop(event) {
     event.preventDefault();
     return false;
+}
+
+
+function isValidCombination(countryClassName, flag) {
+    if (countryClassName == "eu7" && flag == "BulgariaFlag") {
+        return true;
+    }
+    if (countryClassName == "eu13" && flag == "FranceFlag") {
+        return true;
+    }
+    if (countryClassName == "eu14" && flag == "FinlandFlag") {
+        return true;
+    }
+    if (countryClassName == "eu16" && flag == "GermanyFlag") {
+        return true;
+    }
+    if (countryClassName == "eu19" && flag == "IcelandFlag") {
+        return true;
+    }
+    if (countryClassName == "eu22" && flag == "ItalyFlag") {
+        return true;
+    }
+    if (countryClassName == "eu34" && flag == "NorwayFlag") {
+        return true;
+    }
+    if (countryClassName == "eu35" && flag == "PolandFlag") {
+        return true;
+    }
+    if (countryClassName == "eu37" && flag == "RomaniaFlag") {
+        return true;
+    }
+    if (countryClassName == "eu38" && flag == "RussianFederationFlag") {
+        return true;
+    }
+    if (countryClassName == "eu42" && flag == "SpainFlag") {
+        return true;
+    }
+    if (countryClassName == "eu43" && flag == "SwedenFlag") {
+        return true;
+    }
+    if (countryClassName == "eu45" && flag == "TurkeyFlag") {
+        return true;
+    }
+    if (countryClassName == "eu46" && flag == "UkraineFlag") {
+        return true;
+    }
+    if (countryClassName == "eu47" && flag == "UnitedKingdomFlag") {
+        return true;
+    }
 }
