2252a61e3a543b283e67ad3ca61c021646854ee8
luxrobi <luxrobi@yahoo.com>
2017-06-24 15:44:08 +0300

    drag and drop for flags, dialog displays flags

--- application for children/includes/scripts/runEuropeMap.js
+++ application for children/includes/scripts/runEuropeMap.js
@@ -16,8 +16,34 @@ $(document).ready(function(){
         }
     });
 // END OF THE CSSMap;
+
+    //TODO remove this hack
+    //it is used to remove the event listeners from the spans, so that no country can be selected
+    //i hope the map loads in 0.9 sec...
+    setTimeout(function () {
+        $('span.m').each(function (i, obj) {
+            $(obj).unbind("click");
+        });
+        $('.active-region').removeClass("active-region");
+    }, 900);
 });
 
+//this map contains the countries classes (euxy) and the flags for them, with the img path
+var countriesMap = {
+    "eu13": ["FranceFlag", "../includes/flags/Flag_of_France.svg.png"],
+    "eu14": ["FinlandFlag", "../includes/flags/Flag_of_Finland.svg.png"],
+    "eu16": ["GermanyFlag", "../includes/flags/Flag_of_Germany.svg.png"],
+    "eu22": ["ItalyFlag", "../includes/flags/Flag_of_Italy.jpg"],
+    "eu34": ["NorwayFlag", "../includes/flags/Flag_of_Norway.jpg"],
+    "eu35": ["PolandFlag", "../includes/flags/Flag_of_Poland.jpg"],
+    "eu37": ["RomaniaFlag", "../includes/flags/Flag_of_Romania.svg.png"],
+    "eu38": ["RussianFederationFlag", "../includes/flags/Flag_of_Russia.jpg"],
+    "eu42": ["SpainFlag", "../includes/flags/Flag_of_Spain.png"],
+    "eu43": ["SwedenFlag", "../includes/flags/Flag_of_Sweden.svg.png"],
+    "eu45": ["TurkeyFlag", "../includes/flags/Flag_of_Turkey.jpg"],
+    "eu46": ["UkraineFlag", "../includes/flags/Flag_of_Ukraine.jpg"],
+    "eu47": ["UnitedKingdomFlag",  src="../includes/flags/Flag_of_the_United_Kingdom.svg.png"]
+};
 
 //MODAL starts here
 // Get the modal
@@ -26,10 +52,8 @@ var modal = document.getElementById('myModal');
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-};
+// for storing events so that they can be disabled
+var events = {};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -39,18 +63,57 @@ window.onclick = function(event) {
 };
 
 function displayModal(event) {
+
+    if (modal.style.display == "block") {
+        return;
+    }
+
     //open with a delay, so that the user has time to hover over countries
     var delay = setTimeout(function () {
+        //new items should be inserted here
         var grandparent = event.target.parentElement.parentElement;
-        modal.firstElementChild.lastElementChild.innerText = grandparent.className;
+
+        //check if there is a flag, and if so, remove it
+        var df = document.getElementById("dialogFlag");
+        if (df != null) {
+            df.parentElement.removeChild(df);
+        }
+
+        //get the matching flag and insert it dynamically
+        var toInsert = getImageTag(grandparent.className.split(' ')[0]);
+
+        //insert it
+        modal.firstElementChild.appendChild(toInsert);
+
+        //display it
         modal.style.display = "block";
+
+        //also, disable click, hover, etc on background
+        $('#map-europe').addClass("not-active");
     },
         700);
-    //if the user has hovered out
+    //cancel the timeout if the user has hovered out
     this.onmouseout = function () {
         clearTimeout(delay);
+
+        //also, enable click, hover, etc on background
+        $('#map-europe').removeClass("not-active");
     }
 }
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+
+function getImageTag(className) {
+    var img = document.createElement('img');
+    img.id = "dialogFlag";
+    img.className = 'img-in-modal-dialog';
+    img.src = countriesMap[className][1];
+    return img;
+}
+
 //MODAL ends here
 
 //drag and drop starts here
@@ -69,9 +132,13 @@ function drop(event) {
 
     //check if valid
     if (isValidCombination(grandparent.className, flagIdToRemove)) {
+        //hide dragged flag
         $("#" + flagIdToRemove).animate({ opacity: 0 }); //.fadeOut(); //.hide()
+        //color map that it was filled
+        $(grandparent).addClass("correctly-dropped");
     } else {
         alert("invalid drop");
+        //TODO some sound should play here
     }
 }
 
@@ -85,48 +152,6 @@ function disabledDrop(event) {
     return false;
 }
 
-
-//TODO if the clickable map remains, use countryClassName.indexOf("...") !== -1,
-// because there may be cases, where the class looks like "eu7 active-region",
-// and then the drop is invalid
 function isValidCombination(countryClassName, flag) {
-    if (countryClassName == "eu13" && flag == "FranceFlag") {
-        return true;
-    }
-    if (countryClassName == "eu14" && flag == "FinlandFlag") {
-        return true;
-    }
-    if (countryClassName == "eu16" && flag == "GermanyFlag") {
-        return true;
-    }
-    if (countryClassName == "eu22" && flag == "ItalyFlag") {
-        return true;
-    }
-    if (countryClassName == "eu34" && flag == "NorwayFlag") {
-        return true;
-    }
-    if (countryClassName == "eu35" && flag == "PolandFlag") {
-        return true;
-    }
-    if (countryClassName == "eu37" && flag == "RomaniaFlag") {
-        return true;
-    }
-    if (countryClassName == "eu38" && flag == "RussianFederationFlag") {
-        return true;
-    }
-    if (countryClassName == "eu42" && flag == "SpainFlag") {
-        return true;
-    }
-    if (countryClassName == "eu43" && flag == "SwedenFlag") {
-        return true;
-    }
-    if (countryClassName == "eu45" && flag == "TurkeyFlag") {
-        return true;
-    }
-    if (countryClassName == "eu46" && flag == "UkraineFlag") {
-        return true;
-    }
-    if (countryClassName == "eu47" && flag == "UnitedKingdomFlag") {
-        return true;
-    }
+    return countriesMap[countryClassName.split(' ')[0]][0] == flag;
 }
